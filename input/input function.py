05da9daac2c78c2deedda610f401898ea48e1424
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-02-13 19:06:49 +0530

    added input function

---
+++ input/input function.py
@@ -0,0 +1,18 @@
+a=input("enter u r name:")
+b=int(input("enter u age:"))
+print("I'm ",a ,"and i'm",b ,"years old")
+print("hello"+" "+ input("what is ur name?"))
+
+# using formatted string 
+print(f"i'm {a} and i'm {b} years old")
+
+a=input("enter number:")
+b=input("enter text or number:")
+c=str(a)+b
+print(f"the result is {c}")
+
+a=input("enter any number:")
+b=input("enter any number:")
+c=int(a)+int(b)
+print(f"the total sum is {c}")
+
