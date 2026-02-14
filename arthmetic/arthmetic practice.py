723936fc0e7077d4897c68f95a551e5f2b99f1aa
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-02-14 15:26:33 +0530

    added arthmetic operations

---
+++ arthmetic/arthmetic practice.py
@@ -0,0 +1,32 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter the number:"))
+add=a+b
+print(f"the addition is {add}")
+#subtraction
+sub=a-b
+print(f"the subtraction is {sub}")
+#multiplication
+mul=a*b
+print(f"the multiplication is {mul}")
+#division
+div=a/b
+print(f"the division is {div}")
+#modulus
+mod=a%b
+print(f"the modulus is {mod}")
+#exponent
+exp=a**b
+print(f"the exponent is {exp}")
+#floor division
+floor=a//b
+print(f"the floor division is {floor}")
+#bodmas
+res=10+3*2
+print(res)
+#negative numbers
+a=-10
+b=5
+print(a//b)
+print(a%b)
+
+
