4769eb7c5c3707832cc7e373dc6fd3e0a2992b72
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-02-15 17:12:27 +0530

    added about comparision operators

---
+++ comparision/practice.py
@@ -0,0 +1,20 @@
+'''comparision operators are used to compare two values. They return true or falses
+#comparision operators are <,>,==,>=,<==,!='''
+x=100
+print(x==100)#checks if two values are equal
+#not equal to
+x=int(input("Enter a number:"))
+print(x!=5)
+#greater than
+print(8>3)
+#less than
+print(2<10)
+#greater or equal to
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+print(a>=b)
+#less than or equal to
+num_1=int(input("enter a number:"))
+num_2=int(input("enter a number:"))
+print(num_1<=num_2)
+
