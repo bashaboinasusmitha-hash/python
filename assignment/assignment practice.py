2b5e4545968029254df29704afbc133b1465751c
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-02-15 16:00:32 +0530

    added assignment operations

---
+++ assignment/assignment practice.py
@@ -0,0 +1,32 @@
+#(=)is used to assign value to variable
+a=10
+b="susmitha"
+print(a,b)
+#(+=) is used to add and assign value shortend operator
+a=0
+a+=5
+a=a+5
+print(a)
+#(-=) is used to subtract 
+a=10
+a-=5
+print(a)
+#assigning different values to different variables at a time
+d,e,f=10,3,6
+print(d,e,f)
+#(/=)
+a=15
+a/=3
+print(a)
+#(//=)
+a=20
+a//=3
+print(a)
+#(**=)
+a=2
+a**=3
+print(a)
+#(%=)
+a=20
+a%=3
+print(a)
