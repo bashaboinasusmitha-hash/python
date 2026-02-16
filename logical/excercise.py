f37b3b5926181a7f5525ee6563b566330c529d2f
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-02-16 14:52:30 +0530

    added problems on logical operators

---
+++ logical/excercise.py
@@ -0,0 +1,22 @@
+#problems
+a=20
+b=40
+print(a<30 and b>38)
+print(a>30 and b<38)
+print(a>30 or b<38)
+print(a<30 or b>38)
+print(not(a<30))
+print(not(b))
+print(not(a))
+c=False
+print(not(c))
+#problems on advanced concepts
+x=5
+y=0
+z=10
+print(x and y or z)
+print(5 or 0 and 10)
+print(True or False and True)
+print(not True or False and True)
+print(1 and 0 or 5 and 6)
+print(0 or 3 and 4 or 5 and 0)
