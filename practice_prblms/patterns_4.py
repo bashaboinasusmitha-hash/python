cb012821ab1c207938970db066ea1ee723e56544
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-03-05 21:16:06 +0530

    added patterns

--- practice_prblms/patterns_4.py
+++ practice_prblms/patterns_4.py
@@ -5,7 +5,6 @@ for i in range(r):
     for j in range(i):
         print("*",end="")
     print()
-
 a=int(input("enter the no.of rows:"))
 b=int(input("enter the no. of columns:"))
 b1=a-1
@@ -13,7 +12,6 @@ for i in range(a):
     for j in range(i+1):
         print('*',end="")
     print()
-
 r=7
 c=10
 c1=r-1
@@ -24,7 +22,6 @@ for i in range(r):
     for k in range(temp):
         print("*",end="")
     print()
-
 r=7
 c=10
 c1=r-1
@@ -79,31 +76,6 @@ for i in range(r):
         print(" ",end="")
     temp=i*2+1
     for j in range(temp):
-        if j==0 or j==i*2 or i==r-1:
-            print("*",end="")
-        else:
-            print(" ",end="")
-    print()
-r=5
-c=7
-c1=r-1
-for i in range(r-1,-1,-1):
-    for j in range(c1-i):
-        print(" ",end="")
-    temp=i*2+1
-    for j in range(temp):
-        if i==c1 or j==i*2 or j==0:
-            print("*",end="")
-        else:
-            print(" ",end="")
-    print()
-r=5
-c=7
-c1=r-1
-for i in range(r-1):
-    for j in range(c1-i):
-        print(" ",end="")
-    for j in range(i*2+1):
         if j==0 or j==i*2 or i==r-1:
             print("*",end="")
         else:
