7e1c867c9e4caae5ae754a09bceefb256f78a71b
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-03-07 22:05:19 +0530

    added functions

---
+++ practice_prblms/sample_prblms.py
@@ -0,0 +1,44 @@
+s="susmitha"
+b=4
+for i in range(b):
+    print(s)
+def fun(s,n):
+    for i in range(n):
+        print(s)
+fun("susmitha",3)
+fun("sushma",4)
+def wishes():
+    print("good morning")
+    print("good evng")
+wishes()
+def string(n):
+    print("Tharun")
+    for i in range(n):
+        print("ravinder")
+    print("ramadevi")
+string(3)
+string(2)
+def name(a,n):
+    print(a,n)
+    print(n,a)
+name("ram",2)
+name("sita",3)
+def name(a,n):
+    for i in range(n):
+        print(a)   
+name("ram",2)
+name("sita",3)
+def fun():
+    print("hello")
+print("code started")
+fun()
+print("code ended")
+def arg():
+    print("hello guys")
+    return 8
+var=arg()
+print(var)
+def arg(n):
+    print("hello guys")
+    return n
+print(arg(10))
