4dc476f60aac8794f154ca980e9ddda6449c2efd
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-04-12 19:18:19 +0530

    added program

--- leetcode/length of last word.py
+++ leetcode/length of last word.py
@@ -1,7 +1,7 @@
 #Length of last word:
 '''Given a string s consisting of words and spaces, return the length of the last word in the string.
 A word is a maximal substring consisting of non-space characters only.'''
-s= "wood hello wood"
+s= "   fly me   to   the moon  "
 s=s.split()
 n=len(s)
 li=[]
@@ -14,3 +14,9 @@ print(m)
 for i in range(m):
     if li[i]==li[m-1]:
         print(len(li[m-1]))
+s="hello world"
+s=s.split()
+print(s)
+for word in s:
+    if word==s[-1]:
+        print(len(word))
