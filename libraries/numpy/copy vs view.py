0db1664e93835135bb0e443286d671c0851560b7
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-04-14 13:06:05 +0530

    added about copy and view

--- libraries/numpy/copy vs view.py
+++ libraries/numpy/copy vs view.py
@@ -8,6 +8,9 @@ x=arr.copy()
 arr[0]=42
 print(arr)#[42 2 3 4 5]
 print(x)#[1 2 3 4 5] here there is no changes as chages made in arr
+x[1]=40
+print(x)#[1 40 3 4 5]
+print(arr)#[42 2 3 4 5]
 y=arr.view()
 arr[0]=20
 print(arr)#[20  2  3  4  5]
@@ -29,5 +32,6 @@ arr[1]=2
 x[2]=4
 print(x)#[1 3 4 7 8]
 print(arr)#[1 2 5 7 8]
+print(y)#[1 2 5 7 8]
 print(x.base)#None
 print(y.base)#[1 2 5 7 8]
