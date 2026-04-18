a9d203a879f3485a172a5e49e1e81c2ad2c860df
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-04-18 08:38:40 +0530

    added program

---
+++ leetcode/power(x,n).py
@@ -0,0 +1,11 @@
+#Power(x,n)
+'''Implement pow(x, n), which calculates x raised to the power n (i.e., xn).'''
+x=2.00000
+n=10
+print(x**n)#1024.0
+x=2.00000
+n=-2
+print(x**n)#0.25
+x=2.10000
+n=3
+print(x**n)#9.26100
