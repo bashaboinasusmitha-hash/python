da7eb59ba377ed9190edac0e11d66d01de05ca7f
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-04-19 21:26:12 +0530

    added program

---
+++ leetcode/sum multiples.py
@@ -0,0 +1,11 @@
+#Sum multiples:
+'''Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
+Return an integer denoting the sum of all numbers in the given range satisfying the constraint.'''''
+n=9
+sum=0
+for i in range(1,n+1):
+    if i%3==0 or i%5==0 or i%7==0:
+        print(i)
+        sum+=i
+    
+print(sum)
