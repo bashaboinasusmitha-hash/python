db126eb2744e7e8ea69ce2825d13d48253ada367
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-04-26 23:01:29 +0530

    added program

---
+++ leetcode/valid palindrome.py
@@ -0,0 +1,19 @@
+#Valid Palindrome:
+'''A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and
+ removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.'''
+s="A man, a plan, a canal: Panama"
+s=s.lower()
+n=len(s)
+res=""
+for i in range(n):
+    if s[i].isalnum():
+        res+=s[i]
+ans=""
+m=len(res)
+for i in range(m-1,-1,-1):
+    ans+=res[i]
+if res==ans:
+    print(True)
+else:
+    print(False)
