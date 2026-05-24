0d93d7d01db7dafc52447413d834106378dc6495
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-05-24 18:36:00 +0530

    added programs

--- dailypracticeprograms/practice_programs_6.py
+++ dailypracticeprograms/practice_programs_6.py
@@ -42,3 +42,44 @@ for i in s:
     else:
         dic[i]+=1
 print(dic)
+#Conditional Statements
+#Check whether a person is eligible to vote.:
+age=int(input("enter u r age:"))
+if age>=18:
+    print("U r eligible for voting!")
+else:
+    print("Not eligible for voting!")
+#Find greatest of three numbers.
+li=[20,31,21]
+maximum=li[0]
+for i in li:
+    if i>maximum:
+        maximum=i
+print(maximum)
+letter=input("Enter a letter:").lower()
+vowels="aeiou"
+if letter in vowels:
+    print("letter is vowel")
+else:
+    print("letter is consonant")
+#Create a simple calculator using if-elif.
+symbol=input("Enter a symbol:")
+if symbol == "+":
+    print("addition")
+elif symbol== "-":
+    print("subtraction")
+elif symbol == "*":
+    print("multiplication")
+elif symbol=="%":
+    print("modulo")
+elif symbol=="/":
+    print("division")
+#Check whether a number is positive, negative, or zero.
+number=int(input("Enter a number:"))
+if number==0:
+    
+    print("number is zero")
+elif number>0:
+    print("number is positive")
+elif number<0:
+    print("number is negative")
