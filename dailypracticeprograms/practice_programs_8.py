f33f4fc2d80a40760d5f347fab5c0eaf25c83dd3
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-05-29 17:59:46 +0530

    added programs

--- dailypracticeprograms/practice_programs_8.py
+++ dailypracticeprograms/practice_programs_8.py
@@ -21,3 +21,34 @@ def fact(number):
     else:
         return number*fact(number-1)
 print(fact(5))
+#Write a function to find largest element in a list.
+def largest(*kwags):
+    maxi=kwags[0]
+    for i in range(len(kwags)):
+        if kwags[i]>maxi:
+            maxi=kwags[i]
+    return maxi
+print(largest(2,5,3,7,8,9))
+'''Write a lambda function for:
+Square of number
+Even or odd
+Maximum of two numbers'''
+square=lambda x:x**2
+print(square(2))
+even_odd=lambda x:"even" if x%2==0 else "odd"
+print(even_odd(6))
+max_min=lambda x,y: x if x>y else y
+print(max_min(5,7))
+#Write a function with default arguments.
+def default(a,b="susmitha"):
+    print(a,b)
+ans=default(2)
+#Write a function using *args.
+def add(*args):
+    a=sum(args)
+    return a
+print(add(2,3,5,6))
+#Write a function using **kwargs.
+def details(**kwags):
+    return kwags
+print(details(name="susmitha",roll_no=6624))
