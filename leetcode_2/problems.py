c7455d9c09cc3461e31df23dfe3d6dc740a07659
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-07-20 17:15:50 +0530

    added a program of finding the elements which number is smaller than current element

---
+++ leetcode_2/problems.py
@@ -0,0 +1,10 @@
+#find the number of elements which are smaller than current element.
+nums=[8,1,2,2,3]
+ans=[]
+for i in range(len(nums)):
+    count=0
+    for j in range(len(nums)):
+        if nums[i]>nums[j]:
+            count+=1
+    ans.append(count)
+print(ans)
