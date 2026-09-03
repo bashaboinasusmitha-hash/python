ce6c1f2f6199320cd4d6eff3b535893919b904b8
bashaboinasusmitha-hash <bashaboinasusmitha@gmail.com>
2026-09-03 15:09:56 +0530

    added about semicolon in python

--- concepts/semicolon_optional.py
+++ concepts/semicolon_optional.py
@@ -1,2 +1,8 @@
 #Semicolon (optional rarely used in Python)
 print("hello world");print("hello susmitha");print("how r u ?");
+'''hello world
+hello susmitha
+how r u ?'''
+'''this helps us to write the multiple statements in a single line by using semicolon,
+but it is not recommended to use semicolon in python because it reduces the readability of the code.'''
+#print("hello world") print("hello susmitha") print("how r u ?") this will give an error because we have not used semicolon to separate the statements.
